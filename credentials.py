34c97e9cb9448a0dfdf6fac21abbef783df68184
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-04-23 23:55:35 +0300

    create class and instance variables

---
+++ credentials.py
@@ -0,0 +1,18 @@
+class Credentials:
+    """
+    Class that generates new instances of credentials
+    """
+
+    def __init__(self,account,user_name,password):
+
+        '''
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+            account: New credentials account.
+            user_name: New credentials user name.
+            password : New credentials password.
+        '''
+        self.account = account
+        self.user_name = user_name
+        self.password = password    
