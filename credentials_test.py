4166ba7c39abf030fbd3a3fcd44304e531593f68
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-04-24 00:21:28 +0300

    add a method to create a new instance and test that objects are instantiated correctly

--- credentials_test.py
+++ credentials_test.py
@@ -3,9 +3,28 @@ from credentials import Credentials
 
 class TestCredentials(unittest.TestCase):
 
-  '''
-  Test class that defines test cases for the credentials class behaviours.
+    '''
+    Test class that defines test cases for the credentials class behaviours.
 
-  Args:
-    unittest.TestCase: TestCase class that helps in creating test cases
-  '''
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_credentials = Credentials("GitHub","Miss-Faith","***") # create credentials object
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_credentials.account,"GitHub")
+        self.assertEqual(self.new_credentials.user_name,"Miss-Faith")
+        self.assertEqual(self.new_credentials.password,"***")
+
+
+if __name__ == '__main__':
+    unittest.main()
