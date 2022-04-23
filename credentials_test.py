f7f71429abe10f5986a5b61335354328f7bda7cb
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-04-24 00:06:29 +0300

    create test framework file, import credentials class, create test subclass

---
+++ credentials_test.py
@@ -0,0 +1,11 @@
+import unittest
+from credentials import Credentials
+
+class TestCredentials(unittest.TestCase):
+
+  '''
+  Test class that defines test cases for the credentials class behaviours.
+
+  Args:
+    unittest.TestCase: TestCase class that helps in creating test cases
+  '''
