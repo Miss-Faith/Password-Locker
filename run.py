ba63e072aa9d98707ac5f42fc9905be7711c10a4
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-04-24 02:54:25 +0300

    add create credentials in run file

--- run.py
+++ run.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3.8
 
+from credentials import Credentials
+
+def create_credentials(account,user,password):
+    '''
+    Function to create a new credential
+    '''
+    new_credentials = Contact(account,user,password)
+    return new_credentials
