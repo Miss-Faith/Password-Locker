b7faff7e9010b1ae320343f5cd1200ccde72d63d
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-04-24 02:59:20 +0300

    add functions that implement behaviours created

--- run.py
+++ run.py
@@ -6,5 +6,35 @@ def create_credentials(account,user,password):
     '''
     Function to create a new credential
     '''
-    new_credentials = Contact(account,user,password)
+    new_credentials = credentials(account,user,password)
     return new_credentials
+
+def save_credentials(credentials):
+    '''
+    Function to save credentials
+    '''
+    credentials.save_credentials()
+
+def del_credentials(credentials):
+    '''
+    Function to delete a credential
+    '''
+    credentials.delete_credentials()
+
+def find_credentials(account):
+    '''
+    Function that finds a credentials by number and returns the credentials
+    '''
+    return Credentials.find_by_account(account)
+
+def check_existing_credentials(account):
+    '''
+    Function that check if a credential exists with that account and return a Boolean
+    '''
+    return Credentials.credentials_exist(account)
+
+def display_credentials():
+    '''
+    Function that returns all the saved credentials
+    '''
+    return Credentials.display_credentials()
