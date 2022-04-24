b0b3eface4440ecda45c0a07eae26349f4231340
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-04-24 18:50:18 +0300

    add test for multiple users and teardown method after each test

--- user_test.py
+++ user_test.py
@@ -29,7 +29,22 @@ class TestUser(unittest.TestCase):
         test_save_user test case to test if the user object is saved into the user list
         '''
         self.new_user.save_user() # saving the new user
-        self.assertEqual(len(User.user_list),1)   
+        self.assertEqual(len(User.user_list),1)  
+
+    def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        User.user_list = []
+
+    def test_save_multiple_user(self):
+        '''
+        test_save_multiple_users to check if we can save multiple users objects to our user_list
+        '''
+        self.new_user.save_user()
+        test_user = User("UserName","UserPassword") # new user
+        test_user.save_user()
+        self.assertEqual(len(User.user_list),2) 
 
 if __name__ == '__main__':
     unittest.main() 
